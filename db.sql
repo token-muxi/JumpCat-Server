ba222099be6b0bd4809de2aafd27641b137dfe8d
zhxy-CN <admin@owo.cab>
2025-05-24 19:15:17 +0800

    :bug: fix: 修复 p2 不能为空值的问题

--- db.sql
+++ db.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `room` (
     `room` int NOT NULL,
     `p1` text NOT NULL,
-    `p2` text NOT NULL,
+    `p2` text,
     `is_start` bool NOT NULL,
     `map` json NOT NULL,
     PRIMARY KEY (`room`)
