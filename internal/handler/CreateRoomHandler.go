a66bdc57b5c2476992204b8a42902679383b7bde
becky200605 <lidehua0821@gmail.com>
2025-05-24 19:11:39 +0800

    feat: 增加创建房间功能 (#1)

--- internal/handler/CreateRoomHandler.go
+++ internal/handler/CreateRoomHandler.go
@@ -1,9 +1,37 @@
 package handler
 
 import (
+	"JumpCat-Server/internal/database"
+	"JumpCat-Server/internal/repository"
+	"JumpCat-Server/internal/service"
+	"JumpCat-Server/internal/util"
+	"encoding/json"
 	"net/http"
 )
 
-func CreateRoom(w http.ResponseWriter, r *http.Request) {
+type CreateRoomRequest struct {
+	Palyer1UUid string `json:"uuid"`
+}
 
+type CreateRoomReponse struct {
+	RoomID int `json:"room_id"`
+}
+
+func CreateRoom(w http.ResponseWriter, r *http.Request) {
+	db := database.GetDB()
+	rr := repository.NewRoomRepository(db)
+	s := service.NewCreateRoomService(rr)
+	var request CreateRoomRequest
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		util.WriteResponse(w, http.StatusBadRequest, "invalid request")
+		return
+	}
+	Room, err := s.CreateRoom(request.Palyer1UUid)
+	if err != nil {
+		util.WriteResponse(w, http.StatusInternalServerError, "failed to create room")
+		return
+	}
+	util.WriteResponse(w, http.StatusOK, CreateRoomReponse{
+		RoomID: Room,
+	})
 }
